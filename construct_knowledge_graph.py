b50938a0595944ddaec1784f8e6cfbb266352dc5
Damien <damien.morard@etu.unige.ch>
2017-11-30 16:02:38 +0100

    Add improvement for rdf and a query example

--- construct_knowledge_graph.py
+++ construct_knowledge_graph.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import rdflib
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
 from rdflib.namespace import DC, FOAF
 import extract_abstract as ex_ab
@@ -20,35 +21,60 @@ def create_rdf_graph(config):
     rdf_graph = Graph()
 
     vgiid = Namespace('http://vgibox.eu/repository/index.php?curid=')
-    my_namespace = Namespace('http://cui.unige.ch/')
+    cui = Namespace('http://cui.unige.ch/')
 
     rdf_graph.bind("vgiid", vgiid)
     rdf_graph.bind("foaf", FOAF)
     rdf_graph.bind("dc", DC)
+    rdf_graph.bind("cui", cui)
 
     config_abstract = 'config/config_extract.yml'
 
     for doc_id, dic_content in ex_ab.extract_abstracts(config_abstract):
         title = dic_content[doc_id]['title']
         description = dic_content[doc_id]['abstract']
-        rdf_graph.add((vgiid[doc_id], RDF.type, my_namespace.article))
+        rdf_graph.add((vgiid[doc_id], RDF.type, cui.article))
         rdf_graph.add((vgiid[doc_id], DC['title'], Literal(title)))
         rdf_graph.add((vgiid[doc_id], DC['description'], Literal(description)))
 
-        for keyword in dic_content[doc_id]['keywords'].split():
-            print(keyword)
-            rdf_graph.add((vgiid[doc_id], DC['keywords'], Literal(keyword)))
+        for keyword in dic_content[doc_id]['keywords']:
+            rdf_graph.add((vgiid[doc_id], DC['subject'], Literal(keyword)))
 
     rdf_normalized = rdf_graph.serialize(format='n3')
-    rdf_normalized = rdf_normalized.decode("utf-8")
+    rdf_normalized = rdf_normalized.decode('utf-8')
 
-    write_rdf(config['rdf_file'], rdf_normalized)
+    # write_rdf(config['rdf_file'], rdf_normalized)
+
+def query_example():
+    """Just to keep an example query, not for using"""
+    rdf_graph = Graph()
+
+    vgiid = Namespace('http://vgibox.eu/repository/index.php?curid=')
+    cui = Namespace('http://cui.unige.ch/')
+
+    rdf_graph.bind("vgiid", vgiid)
+    rdf_graph.bind("foaf", FOAF)
+    rdf_graph.bind("dc", DC)
+    rdf_graph.bind("cui", cui)
+    rdf_graph.add((vgiid['919'], RDF.type , cui.article))
+
+    qres = rdf_graph.query(
+    """
+    PREFIX cui:<http://cui.unige.ch/>
+    SELECT DISTINCT ?a
+       WHERE {
+          ?a a cui:article .
+       }""")
+
+    for row in qres:
+        print(row)
 
 
 if __name__ == '__main__':
 
     config = gf.load_config('config/config_rdf.yml')
-    create_rdf_graph(config)
+    # create_rdf_graph(config)
+    query_example()
 
     """config_abstract = 'config/config_extract.yml'
     for lol1, lol2 in ex_ab.extract_abstracts(config_abstract):
