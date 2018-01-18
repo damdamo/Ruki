b97c186b089d351bf7856abfa99c37fefb972b9f
Damien <damien.morard@etu.unige.ch>
2018-01-18 12:05:05 +0100

    Add methods to have some options in first line of a file

--- add_method_knowledge_graph.py
+++ add_method_knowledge_graph.py
@@ -5,12 +5,49 @@ from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
 from rdflib.namespace import DC, FOAF
 import extract_abstract as ex_ab
 import generic_functions as gf
+import re
 
 def write_rdf(file_name, rdf):
     """A simple function to write rdf in a file"""
     with open(file_name, 'w') as output_file:
         output = '{}'.format(rdf)
         output_file.write("{}".format(output))
+
+def extract_options(line):
+    """Take a line and extract options to add
+    informations. We have special tag for this like
+    method_name, informations..."""
+
+    method_name = extract_tag('method_name', line)
+    method_informations = extract_tag('method_informations', line)
+
+    dic = {}
+
+    print(method_name)
+    print(method_informations)
+
+
+    # method_name = line[pos_method_name[0], pos_method_name[1]]
+    # method_informations = line[pos_method_informations[0], pos_method_informations[1]]
+
+    #regex_get_abstract = re.compile('{(.)*}( )?')
+    #abstract = re.sub(regex_get_abstract, '', content_mod)
+
+def extract_tag(tag_name, line):
+    """Allow to extract a tag for extract_options
+    """
+
+    # Method to keep our tags with regex
+    pattern_name = re.compile(tag_name + "( )?=( )?{(.)*}")
+    content = re.search(pattern_name, line)
+    final_tag = ''
+
+    if content is not None:
+        pos = content.span()
+        final_tag = line[pos[0]:pos[1]]
+
+    return final_tag
+
 
 def parse_file(file_name):
     """This method can parse the file with all data
@@ -24,13 +61,18 @@ def parse_file(file_name):
 
     # We give an id for every cluster
     cluster_count = 0
+    first_line = True
 
     with open(file_name, 'r') as file_reading:
         for line in file_reading:
-            dic_informations[cluster_count] = []
-            for element in line.split():
-                dic_informations[cluster_count].append(element)
-            cluster_count = cluster_count + 1
+            if first_line and line[0:1]=='\\':
+                extract_options(line)
+                first_line = False
+            else:
+                dic_informations[cluster_count] = []
+                for element in line.split():
+                    dic_informations[cluster_count].append(element)
+                cluster_count = cluster_count + 1
 
     return dic_informations
 
