ab0c53af9035fe4ad9645ab8344352d200cc9ed0
Damien <damien.morard@etu.unige.ch>
2018-04-11 10:30:29 +0200

    First modification to change vizualisation

--- website/main.py
+++ website/main.py
@@ -11,16 +11,13 @@ import python_script.import_export_rdf4j as imp_exp
 
 METHOD_FOLDER = 'static/method_schema'
 UPLOAD_FOLDER = '/home/damien/Workspace/master_project/website/temp'
+#URL_SERVER = 'http://kr.unige.ch:8080/rdf4j-server/repositories/master_project_damien'
 ALLOWED_EXTENSIONS = set(['json', 'yml'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-
-@app.route('/coucou')
-def dire_coucou():
-    return 'Coucou !'
 
 @app.route('/')
 def racine():
@@ -54,20 +51,19 @@ def upload_file():
             full_path_rdf_name = full_path_name.replace(extension, '.rdf')
 
             # We take the file and convert it into rdf file
-            add_method.create_rdf_graph(full_path_name, full_path_rdf_name)
+            [_, method_name] = add_method.create_rdf_graph(full_path_name, full_path_rdf_name)
 
             # We take the new rdf graph and we import it in the rdf4j server
-            imp_exp.import_rdf_to_rdf4j(full_path_rdf_name)
+            imp_exp.import_rdf_to_rdf4j(full_path_rdf_name, method_name)
+
+            # We get a new json file for the vizualisation
+            visualization.write_informations_for_visualization(method_name)
 
             flash('Le fichier a correctement été ajouté à la base de connaissance')
             return redirect(url_for('upload_file'))
     else:
         return render_template('import.html')
 
-
-@app.errorhandler(404)
-def ma_page_404(error):
-    return "This is an error 404", 404
 
 @app.route('/world', methods=['GET', 'POST'])
 def display_world():
@@ -80,10 +76,15 @@ def display_world():
         # We insert element selected in first position and we pop the old position
         list_method.insert(0, method_select)
         list_method.pop(index+1)
-
+        print(name_file)
         return render_template('world.html', printable=True, list_method=list_method, name_file=name_file, titre="Bienvenue !")
     else:
         return render_template('world.html', printable=False, list_method=list_method, titre="Bienvenue !")
+
+
+@app.errorhandler(404)
+def ma_page_404(error):
+    return "This is an error 404", 404
 
 
 def allowed_file(filename):
